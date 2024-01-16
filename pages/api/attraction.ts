c2559ab01e5f9467433bd493e4484a4cdf39187a
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-01-16 09:19:01 +0700

    connect database

---
+++ pages/api/attraction.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest,NextApiResponse } from "next";
+
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE
+});
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    connection.query(
+        // 'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+        'SELECT * FROM `attractions` ',
+        function(err:any,results:any){
+            res.status(200).json(results)
+}
+      );  
+}
